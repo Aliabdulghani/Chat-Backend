127ab1b6ee20c8b737c258a37aeac5f899701f1e
Aliabdulghani <20dcd20@gmail.com>
2025-04-05 22:33:26 +0300

    Update

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,11 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (userId) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
-    return token;  // فقط قم بإرجاع التوكن
-}
+    try {
+        const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        return token;
+    } catch (error) {
+        console.error('Error generating token:', error.message);
+        throw new Error('Failed to generate token');
+    }
+};
